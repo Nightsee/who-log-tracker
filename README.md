c2095bf4ec724ecf21213b97fdbaaeec7fb426fb
Nightsee <younes.naitsi.2002@gmail.com>
2023-12-22 19:16:32 +0100

    who_log_tracker v1

---
+++ README.md
@@ -0,0 +1,6 @@
+@nightsee
+## Current Users Logging Script Documentation
+
+### Purpose:
+The purpose of this script is to log information about currently logged-in users to a specified log file. The script retrieves the output of the `who` command, extracts relevant details (username, terminal, login date, and login time), and appends this information to a log file. The log file includes a timestamp indicating when the logging started.
+
---
+++ current_users-script.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+log_file="/home/younes/desktop/service_current_users/current_users_log.txt"
+who_output="$(who)"
+current_date_time=$(date +"started loggin from:  %Y-%m-%d %H:%M:%S")
+username=$(echo $who_output | cut -d' ' -f 1)
+user_terminal=$(echo $who_output | cut -d' ' -f 2)
+login_date=$(echo $who_output | cut -d' ' -f 3)
+login_time=$(echo $who_output | cut -d' ' -f 4)
+
+
+if [ "$(wc -l < "$log_file")" -eq 0 ]; then
+	echo -e "\n\t$current_date_time\n" > $log_file
+	echo -e "\tuser\t\tterminal\t\ŧtime & date" >> $log_file
+	while IFS= read -r line; do
+		username=$(echo $line | cut -d' ' -f 1)
+		user_terminal=$(echo $line | cut -d' ' -f 2)
+		login_date=$(echo $line | cut -d' ' -f 3)
+		login_time=$(echo $line | cut -d' ' -f 4)
+		echo -e "\t$username\t\t$user_terminal\t\t$login_time  $login_date" >> $log_file
+	done <<< "$who_output"
+else
+	while IFS= read -r line; do
+                username=$(echo $line | cut -d' ' -f 1)
+                user_terminal=$(echo $line | cut -d' ' -f 2)
+                login_date=$(echo $line | cut -d' ' -f 3)
+                login_time=$(echo $line | cut -d' ' -f 4)
+                echo -e "\t$username\t\t$user_terminal\t\t$login_time  $login_date" >> $log_file
+        done <<< "$who_output"
+fi
+
+#echo $who_output
+
+#printf "%s\n" "$who_output"
